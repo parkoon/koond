3a3fa7d78ea3ee5657bbafed5389e054bdcb1b99
parkoon <parkoon@users.noreply.github.com>
2024-06-13 07:22:43 +0000

    deploy: b71c3053043c962c111f98a22c7839277cc7f48e

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+import { JSX as JSX_2 } from 'react/jsx-runtime';
+
+export declare const Button: ({ children }: ButtonProps) => JSX_2.Element;
+
+declare type ButtonProps = {
+    children: React.ReactNode;
+};
+
+export { }
