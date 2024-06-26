e85b29b7496b5b85654c5b73f79b1c6afe60ed48
jh.park <jh.park@ent-bc.com>
2024-06-26 10:08:23 +0900

    feat: add prefix & sufixx

--- lib/components/text-input.tsx
+++ lib/components/text-input.tsx
@@ -2,21 +2,30 @@ import * as React from 'react'
 
 import { cn } from '../utils/cn'
 
-export type InputProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'>
+export type InputProps = {
+  prefix?: string | React.ReactNode
+  suffix?: string | React.ReactNode
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'>
 
-const TextInput = React.forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => {
-  return (
-    <input
-      type="text"
-      className={cn(
-        'flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm transition-colors placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
-        className,
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
-})
+const TextInput = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, prefix, suffix, ...props }, ref) => {
+    return (
+      <label className="flex h-10 w-full items-center gap-2 overflow-hidden rounded-md border border-input bg-transparent px-3 text-sm">
+        {prefix}
+        <input
+          type="text"
+          className={cn(
+            'h-full w-full placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        {suffix}
+      </label>
+    )
+  },
+)
 TextInput.displayName = 'TextInput'
 
 export default TextInput
