df6e4b06b3d430efdc5232e84124c496f0752ab9
jh.park <jh.park@ent-bc.com>
2024-06-26 09:41:18 +0900

    fix: max width

--- lib/components/alert-dialog.tsx
+++ lib/components/alert-dialog.tsx
@@ -1,25 +1,25 @@
-import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
-import React from "react";
+import * as AlertDialogPrimitive from '@radix-ui/react-alert-dialog'
+import React from 'react'
 
-import { Button, ButtonProps } from "./button";
-import { cn } from "../utils/cn";
+import { cn } from '../utils/cn'
+import { Button, ButtonProps } from './button'
 
 type AlertDialogProps = {
-  title?: string;
-  description?: string | React.ReactNode;
-  open?: boolean;
-  onOk?(): void;
-  onCancel?(): void;
-  okText?: string;
-  cancelText?: string;
-  okButtonProps?: ButtonProps;
-  cancelButtonProps?: ButtonProps;
-  hideCancelButton?: boolean;
+  title?: string
+  description?: string | React.ReactNode
+  open?: boolean
+  onOk?(): void
+  onCancel?(): void
+  okText?: string
+  cancelText?: string
+  okButtonProps?: ButtonProps
+  cancelButtonProps?: ButtonProps
+  hideCancelButton?: boolean
   classNames?: {
-    title?: string;
-    description?: string;
-  };
-};
+    title?: string
+    description?: string
+  }
+}
 
 const AlertDialog = ({
   title,
@@ -31,27 +31,24 @@ const AlertDialog = ({
   okButtonProps,
   cancelButtonProps,
   hideCancelButton = false,
-  okText = "Confirm",
-  cancelText = "Cancel",
+  okText = 'Confirm',
+  cancelText = 'Cancel',
 }: AlertDialogProps) => {
   return (
     <AlertDialogPrimitive.Root open={open}>
       <AlertDialogPrimitive.Portal>
         <AlertDialogPrimitive.Overlay className="fixed inset-0 z-50 bg-black/40 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
-        <AlertDialogPrimitive.Content className="fixed left-[50%] top-[50%] z-50 w-full max-w-lg translate-x-[-50%] translate-y-[-50%] border bg-white p-5 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg">
+        <AlertDialogPrimitive.Content className="fixed left-[50%] top-[50%] z-50 w-full max-w-md translate-x-[-50%] translate-y-[-50%] rounded-lg border bg-white p-5 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]">
           {title && (
             <AlertDialogPrimitive.Title
-              className={cn(
-                "text-lg font-bold text-center mb-2",
-                classNames?.title
-              )}
+              className={cn('mb-2 text-center text-lg font-bold', classNames?.title)}
             >
               {title}
             </AlertDialogPrimitive.Title>
           )}
           {description && (
             <AlertDialogPrimitive.Description
-              className={cn("mb-5 text-center", classNames?.description)}
+              className={cn('mb-5 text-center', classNames?.description)}
             >
               {description}
             </AlertDialogPrimitive.Description>
@@ -59,12 +56,7 @@ const AlertDialog = ({
 
           <div className="flex gap-2">
             {!hideCancelButton && (
-              <Button
-                variant="outlined"
-                block
-                onClick={onCancel}
-                {...cancelButtonProps}
-              >
+              <Button variant="outlined" block onClick={onCancel} {...cancelButtonProps}>
                 {cancelText}
               </Button>
             )}
@@ -76,7 +68,7 @@ const AlertDialog = ({
         </AlertDialogPrimitive.Content>
       </AlertDialogPrimitive.Portal>
     </AlertDialogPrimitive.Root>
-  );
-};
+  )
+}
 
-export default AlertDialog;
+export default AlertDialog
