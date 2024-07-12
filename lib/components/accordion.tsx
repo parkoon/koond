7be5ba6d277faf3a36fef6cc7b5c197d82304c84
jh.park <jh.park@ent-bc.com>
2024-07-12 11:08:01 +0900

    feat: accordion

---
+++ lib/components/accordion.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import * as AccordionPrimitive from '@radix-ui/react-accordion'
+import { ChevronDown } from 'lucide-react'
+import * as React from 'react'
+
+import { cn } from '../utils/cn'
+
+export type AccordionProps = React.ComponentProps<typeof AccordionPrimitive.Root>
+export const Accordion = ({ ...props }: AccordionProps) => <AccordionPrimitive.Root {...props} />
+
+export type AccordionItemProps = React.ComponentProps<typeof AccordionPrimitive.Item>
+export const AccordionItem = ({ className, ...props }: AccordionItemProps) => (
+  <AccordionPrimitive.Item className={cn('border-b', className)} {...props} />
+)
+
+export type AccordionTriggerProps = React.ComponentProps<typeof AccordionPrimitive.Trigger>
+export const AccordionTrigger = ({ children, className, ...props }: AccordionTriggerProps) => (
+  <AccordionPrimitive.Header className="flex">
+    <AccordionPrimitive.Trigger
+      className={cn(
+        'flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all [&[data-state=open]>svg]:rotate-180',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+      <ChevronDown className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+    </AccordionPrimitive.Trigger>
+  </AccordionPrimitive.Header>
+)
+
+export type AccordionContentProps = React.ComponentProps<typeof AccordionPrimitive.Content>
+export const AccordionContent = ({ className, children, ...props }: AccordionContentProps) => (
+  <AccordionPrimitive.Content
+    className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm"
+    {...props}
+  >
+    <div className={cn('pb-4 pt-0', className)}>{children}</div>
+  </AccordionPrimitive.Content>
+)
---
+++ lib/stories/accordion.stories.tsx
@@ -0,0 +1,93 @@
+import type { Meta } from '@storybook/react'
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '../components/accordion'
+
+const meta = {
+  title: 'ui/Accordion',
+  component: Accordion,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Accordion>
+
+export default meta
+
+export const Default = () => (
+  <Accordion type="single" collapsible>
+    <AccordionItem value="1">
+      <AccordionTrigger>Lorem ipsum dolor sit 1</AccordionTrigger>
+      <AccordionContent>
+        adipisicing elit. Harum esse minus assumenda maiores earum? Sequi doloribus omnis ratione
+        error, at, commodi laboriosam ullam possimus minus ut saepe! Ad, corrupti dolor!
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="2">
+      <AccordionTrigger>Lorem ipsum dolor sit 2</AccordionTrigger>
+      <AccordionContent>
+        adipisicing elit. Harum esse minus assumenda maiores earum? Sequi doloribus omnis ratione
+        error, at, commodi laboriosam ullam possimus minus ut saepe! Ad, corrupti dolor!
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="3">
+      <AccordionTrigger>Lorem ipsum dolor sit 3</AccordionTrigger>
+      <AccordionContent>
+        adipisicing elit. Harum esse minus assumenda maiores earum? Sequi doloribus omnis ratione
+        error, at, commodi laboriosam ullam possimus minus ut saepe! Ad, corrupti dolor!
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
+)
+export const Multiple = () => (
+  <Accordion type="multiple">
+    <AccordionItem value="1">
+      <AccordionTrigger>Lorem ipsum dolor sit 1</AccordionTrigger>
+      <AccordionContent>
+        adipisicing elit. Harum esse minus assumenda maiores earum? Sequi doloribus omnis ratione
+        error, at, commodi laboriosam ullam possimus minus ut saepe! Ad, corrupti dolor!
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="2">
+      <AccordionTrigger>Lorem ipsum dolor sit 2</AccordionTrigger>
+      <AccordionContent>
+        adipisicing elit. Harum esse minus assumenda maiores earum? Sequi doloribus omnis ratione
+        error, at, commodi laboriosam ullam possimus minus ut saepe! Ad, corrupti dolor!
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="3">
+      <AccordionTrigger>Lorem ipsum dolor sit 3</AccordionTrigger>
+      <AccordionContent>
+        adipisicing elit. Harum esse minus assumenda maiores earum? Sequi doloribus omnis ratione
+        error, at, commodi laboriosam ullam possimus minus ut saepe! Ad, corrupti dolor!
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
+)
+
+export const DefaultOpened = () => (
+  <Accordion type="single" collapsible defaultValue="1">
+    <AccordionItem value="1">
+      <AccordionTrigger>Lorem ipsum dolor sit 1</AccordionTrigger>
+      <AccordionContent>
+        adipisicing elit. Harum esse minus assumenda maiores earum? Sequi doloribus omnis ratione
+        error, at, commodi laboriosam ullam possimus minus ut saepe! Ad, corrupti dolor!
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="2">
+      <AccordionTrigger>Lorem ipsum dolor sit 2</AccordionTrigger>
+      <AccordionContent>
+        adipisicing elit. Harum esse minus assumenda maiores earum? Sequi doloribus omnis ratione
+        error, at, commodi laboriosam ullam possimus minus ut saepe! Ad, corrupti dolor!
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="3">
+      <AccordionTrigger>Lorem ipsum dolor sit 3</AccordionTrigger>
+      <AccordionContent>
+        adipisicing elit. Harum esse minus assumenda maiores earum? Sequi doloribus omnis ratione
+        error, at, commodi laboriosam ullam possimus minus ut saepe! Ad, corrupti dolor!
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
+)
