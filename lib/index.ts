b13d7176be92705cc0a8165b584d14bb8a04ad5b
jh.park <jh.park@ent-bc.com>
2024-07-15 11:02:36 +0900

    fix: build error

--- lib/index.ts
+++ lib/index.ts
@@ -1,19 +1,9 @@
-import "./index.css";
+import * as AlertDialog from './components/alert-dialog'
+import * as AspectRatio from './components/aspect-ratio'
+import * as BottomSheet from './components/bottom-sheet'
+import * as Button from './components/button'
+import * as IconButton from './components/icon-button'
+import * as ScreenDrawer from './components/screen-drawer'
+import './index.css'
 
-import * as Button from "./components/button";
-import * as AlertDialog from "./components/alert-dialog";
-import * as AspectRatio from "./components/aspect-ratio";
-import * as BottomSheet from "./components/bottom-sheet";
-import * as IconButton from "./components/icon-button";
-import * as Screen from "./components/screen";
-import * as ScreenDrawer from "./components/screen-drawer";
-
-export {
-  Button,
-  AlertDialog,
-  AspectRatio,
-  BottomSheet,
-  IconButton,
-  Screen,
-  ScreenDrawer,
-};
+export { Button, AlertDialog, AspectRatio, BottomSheet, IconButton, ScreenDrawer }
--- lib/stories/icon-button.stories.tsx
+++ lib/stories/icon-button.stories.tsx
@@ -1,26 +1,27 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { ArrowDownToDot } from "lucide-react";
-import IconButton from "../components/icon-button";
+import type { Meta, StoryObj } from '@storybook/react'
+import { ArrowDownToDot } from 'lucide-react'
+
+import { IconButton } from '../components/icon-button'
 
 const meta = {
-  title: "ui/IconButton",
+  title: 'ui/IconButton',
   component: IconButton,
   argTypes: {
     variant: {
-      options: ["primary", "secondary", "ghost", "outlined"],
-      control: { type: "radio" },
+      options: ['primary', 'secondary', 'ghost', 'outlined'],
+      control: { type: 'radio' },
     },
   },
-} satisfies Meta<typeof IconButton>;
+} satisfies Meta<typeof IconButton>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
 export const _IconButton: Story = {
   args: {
     loading: false,
     block: false,
-    variant: "outlined",
-    icon: <ArrowDownToDot className="w-4 h-4" />,
+    variant: 'outlined',
+    icon: <ArrowDownToDot className="h-4 w-4" />,
   },
-};
+}
